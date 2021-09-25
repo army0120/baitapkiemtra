2eab75cee71c87ae30a189e137efa89b32c818ec
Nhược Linh <57707315+army0120@users.noreply.github.com>
2021-09-25 10:35:32 +0700

    Update bai1b.py

--- bai1b.py
+++ bai1b.py
@@ -1,5 +1,6 @@
+
 A = [1,1,2,6,8,9,4,5,6,45,34,66,44,37,78]
-j=int(input().strip())
+j=int(input("nhap so:"))
 for i in A:
-    if (i>j):
-        print(i.remove(j))
+    if (i<j):
+        print(i)
